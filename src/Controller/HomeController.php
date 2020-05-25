2ac9cc0387f066b2cd3e9305bdc5c6d95b37d2c7
Antoine <sirpa.antoine@gmail.com>
2020-05-25 22:59:28 +0200

    add new products

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+    
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    
+    /**
      * home controller
      * 
-     * @return unknown
+     * @return 
      */
     public function Home()
     {
+        $product = new Product();
+        $product->setName('t-shirt bio blanc');
+        $product->setDescription('t-shirt fabrique avec des produit biologique issue du commerce equitable, il est de couleur blanc et de taille M');
+        $product->setPrice(15.95);
+
+        $this->em->persist($product);
+        $this->em->flush();
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
